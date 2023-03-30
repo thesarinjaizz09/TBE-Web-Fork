99321c554876c3dee6477e6d46bcc2e654e002f1
Sachin <sachinkshuklaoo7@gmail.com>
2023-03-30 10:14:58 +0530

    UI Showing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
 import { Dialog } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
 
 const navigation = [
   { name: 'Home', href: '#' },
